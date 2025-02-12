9b33168eb320cd4ca02fc18643fc1c4f91c673b4
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2025-02-12 14:48:50 -0600

    Handle packages with no optional dependencies (#3)

--- tests/test_uv_lock.py
+++ tests/test_uv_lock.py
@@ -77,3 +77,32 @@ def test_update_metadata():
         'importlib-resources==6.5.2; python_version < "3.10" and python_full_version == "3.9.*"'
         in dst_metadata["optional-dependencies"]["pinned"]
     )
+
+
+def test_update_metadata_no_optional_deps():
+    metadata = {
+        "dependencies": [
+            "anyio>=4.5.2",
+            "boto3>=1.36.15",
+            "colorama; sys_platform == 'win32'",
+            "exceptiongroup>=1.2.2",
+            "importlib-resources; python_version < '3.10'",
+            "fastapi>=0.115.8",
+        ],
+    }
+    hook = PinnedExtraMetadataHook("fixtures/project", {"extra-name": "pinned"})
+
+    dst_metadata = deepcopy(metadata)
+    hook.update(dst_metadata)
+
+    assert dst_metadata["dependencies"] == metadata["dependencies"]
+
+    assert "boto3==1.36.15" in dst_metadata["optional-dependencies"]["pinned"]
+    assert (
+        'colorama==0.4.6; sys_platform == "win32"'
+        in dst_metadata["optional-dependencies"]["pinned"]
+    )
+    assert (
+        'importlib-resources==6.5.2; python_version < "3.10" and python_full_version == "3.9.*"'
+        in dst_metadata["optional-dependencies"]["pinned"]
+    )
